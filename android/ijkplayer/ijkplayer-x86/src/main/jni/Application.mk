c16e544ad0b993072e58cf872ba4561a87807499
dengxudong-b <dengxudong-b@rongcloud.cn>
2021-05-13 20:08:21 +0800

    添加注释

--- android/ijkplayer/ijkplayer-x86/src/main/jni/Application.mk
+++ android/ijkplayer/ijkplayer-x86/src/main/jni/Application.mk
@@ -1,28 +1,17 @@
-# Copyright (c) 2013-2014 Bilibili
-# copyright (c) 2013-2014 Zhang Rui <bbcallen@gmail.com>
-#
-# This file is part of ijkPlayer.
-#
-# ijkPlayer is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-# ijkPlayer is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with ijkPlayer; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+## Application.mk 指定 ndk-build 的项目级设置
+
 
 APP_OPTIM := release
+# 构建此应用所面向的 Android API 级别，并对应于应用的 minSdkVersion
 APP_PLATFORM := android-9
+# 为指定API生成代码
 APP_ABI := x86
 NDK_TOOLCHAIN_VERSION=4.9
 APP_PIE := false
 
+# 用于此应用的 C++ 标准库。
+# 默认情况下使用 system STL。其他选项包括 c++_shared、c++_static 和 none
 APP_STL := stlport_static
 
 # 关闭编译优化
